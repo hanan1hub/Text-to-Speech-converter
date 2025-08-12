9f170d7be4f6911325dba06d7f7472d017479e53
Hanan Majeed <h.hanan2005hb@gmail.com>
2025-08-12 15:31:15 +0500

    Add files via upload

---
+++ textToSpeechGui/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+
+module com.example.texttospeechgui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires freetts;
+
+
+    opens com.example.texttospeechgui to javafx.fxml;
+    exports com.example.texttospeechgui;
+}
